5d6f58f7b33c5ab6f68040ea8e524654ab05fdfb
Angel Zhou <angel.zhou1016@gmail.com>
2022-11-22 14:46:02 -0500

    updates pet sitter index template

--- src/components/pet_sitter/PetSitterIndex.js
+++ src/components/pet_sitter/PetSitterIndex.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import { restaurantIndex } from '../../api/restaurant'
+import { petSitterIndex } from '../../api/pet_sitter'
 
 const cardContainerLayout = {
     display: 'flex',
@@ -11,40 +11,43 @@ const cardContainerLayout = {
 
 const PetSitterIndex = ({ user, msgAlert }) => {
 
-    // const [allPetSitters, setAllPetSitters] = useState([])
+    const [allPetSitters, setAllPetSitters] = useState([])
 
-    // useEffect(() => {
-    //     restaurantIndex(user)
-    //         .then(res => {
-    //             setAllPetSitters(res.data.petsitters)
-    //         })
-    //         .catch((error) => {
-    //             msgAlert({
-    //                 heading: 'Failure',
-    //                 message: 'Index Failure: ' + error,
-    //                 variant: 'danger'
-    //             })
-    //         })
-    // }, [])
+    useEffect(() => {
+        petSitterIndex(user)
+            .then(res => {
+                setAllPetSitters(res.data.petsitters)
+            })
+            .catch((error) => {
+                msgAlert({
+                    heading: 'Failure',
+                    message: 'Index Failure: ' + error,
+                    variant: 'danger'
+                })
+            })
+    }, [])
 
     // map over all restaurants to produce cards to display each restaurant with an image and link
-    // const restaurantCards = allRestaurants.map(restaurant => (
+    const petSitterCards = allPetSitters.map(petSitter => (
 
-    //     <Card key={restaurant.id} style={{ width: '30rem', margin: 8, backgroundColor: '#f2f6ec' }}>
-    //         <Card.Img variant="top" src={FoodImages[`${restaurant.type}`]} style={{ height: '300px' }} alt={restaurant.type} />
-    //         <Card.Header><b>{restaurant.name}</b> / {restaurant.type}</Card.Header>
-    //         <Card.Body>
-    //             <Card.Text>
-    //                 <Link style={{ color: '#ba7a5f', textDecoration: 'none', fontWeight: 'bold' }} to={`/restaurants/${restaurant._id}`}>View {restaurant.name} </Link>
-    //             </Card.Text>
-    //         </Card.Body>
-    //     </Card>
-    // ))
+        <Card key={petSitter.id} style={{ width: '30rem', margin: 8, backgroundColor: '#f2f6ec' }}>
+            {/* <Card.Img variant="top" src={FoodImages[`${restaurant.type}`]} style={{ height: '300px' }} alt={restaurant.type} /> */}
+            <Card.Header><b>{petSitter.first_name} {petSitter.last_name}</b> </Card.Header>
+            <Card.Body>
+                <Card.Text>
+                    <Link style={{ color: '#ba7a5f', textDecoration: 'none', fontWeight: 'bold' }} to={`/petsitters/${petSitter._id}`}>View details</Link>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    ))
 
     return (
         <>
-            Hello PetSitterIndex
-            <Link to={`/petsitters/petsittershow`}>View PetSitterShow </Link>
+            <h2 className='text-center mt-3'>All Pet Sitters</h2>
+            <div className='container-md text-center' style={cardContainerLayout}>
+
+                {petSitterCards}
+            </div>
         </>
     )
 }
