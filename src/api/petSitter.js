5f9af3773f19bcb211b7043213b54deb77d06c7a
Angel Zhou <angel.zhou1016@gmail.com>
2022-11-22 17:03:54 -0500

    adds axios routes for petsitter

---
+++ src/api/petSitter.js
@@ -0,0 +1,57 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+// CREATE a pet sitter profile - token require
+export const petSitterCreate = (data, user) => {
+	return axios({
+		method: 'POST',
+		url: apiUrl + '/petsitters/',
+		data: {
+			petSitter: data,
+		},
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+	})
+}
+
+// INDEX all petSitters - no token require
+export const petSitterIndex = (user) => {
+	return axios({
+		method: 'GET',
+		url: apiUrl + '/petsitters/'
+	})
+}
+
+// SHOW a petSitter - no token require
+export const petSitterShow = (user, id) => {
+	return axios({
+		method: 'GET',
+		url: apiUrl + '/petsitters/' + id + '/'
+	})
+}
+
+// UPDATE a petSitter - token require
+export const petSitterUpdate = (data, user, id) => {
+	return axios({
+		method: 'PATCH',
+		url: apiUrl + '/petsitters/' + id + '/',
+		data: {
+			petSitter: data,
+		},
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+	})
+}
+
+// DELETE a petSitter - token require
+export const petSitterDelete = (user, id) => {
+	return axios({
+		method: 'DELETE',
+		url: apiUrl + '/petsitters/' + id + '/',
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+	})
+}
