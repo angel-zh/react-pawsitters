b314fafacae1d41e51e14b7e87dcd05ba1ff14f1
Angel Zhou <angel.zhou1016@gmail.com>
2022-11-22 15:54:08 -0500

    adds Sams PetOwner components and api

---
+++ src/api/petOwner.js
@@ -0,0 +1,57 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+// CREATE a pet owner - token require
+export const petOwnerCreate = (data, user) => {
+	return axios({
+		method: 'POST',
+		url: apiUrl + '/petowners/',
+		data: {
+			pet_owner: data,
+		},
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+	})
+}
+
+// INDEX all pet owners - no token require
+export const petOwnerIndex = (user) => {
+	return axios({
+		method: 'GET',
+		url: apiUrl + '/petowners/'
+	})
+}
+
+// SHOW a pet owner - no token require
+export const petOwnerShow = (user, id) => {
+	return axios({
+		method: 'GET',
+		url: apiUrl + '/petowners/' + id
+	})
+}
+
+// UPDATE a pet owner - token require
+export const petOwnerUpdate = (data, user, id) => {
+	return axios({
+		method: 'PATCH',
+		url: apiUrl + '/petowners/' + id,
+		data: {
+			pet_owner: data,
+		},
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+	})
+}
+
+// DELETE a pet owner - token require
+export const petOwnerDelete = (user, id) => {
+	return axios({
+		method: 'DELETE',
+		url: apiUrl + '/petowners/' + id,
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+	})
+}
