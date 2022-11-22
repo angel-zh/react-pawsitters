01615d5dbd5844ac8cf1349c0dc368cd56ef9b00
melanie silva <melsil11@hotmail.com>
2022-11-22 11:40:47 -0500

    created api calls for reviews

---
+++ src/api/review.js
@@ -0,0 +1,50 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+// CREATE a review- token required
+export const reviewCreate = (user, petsitterId, newReview) => {
+	const ownerId = { owner: user._id }
+	const review = {...newReview, ...ownerId}
+
+	return axios({
+		url: `${apiUrl}/reviews/${petsitterId}/`,
+		method: 'POST',
+		data: { review: review }
+	})
+}
+
+// INDEX reviews - no token
+export const reviewIndex = (user) => {
+
+	return axios({
+		url: apiUrl + `/reviews/`,
+		method: 'GET',
+
+	})
+}
+
+// UPDATE a review - token Required
+export const reviewUpdate = (user, petsitterId, updatedReview) => {
+	const ownerId = { owner: user._id }
+	const review = {...updatedReview, ...ownerId}
+
+	return axios({
+		url: `${apiUrl}/reviews/${petsitterId}/${review.id}/`,
+		method: 'PATCH',
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+		data: { review: review }
+	})
+}
+
+// DELETE review - token required
+export const reviewDelete = (user, petsitterId, reviewId) => {
+	return axios({
+		url: `${apiUrl}/reviews/${petsitterId}/${reviewId}/`,
+		method: 'DELETE',
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		}
+	})
+}
