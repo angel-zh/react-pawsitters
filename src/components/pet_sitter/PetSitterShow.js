62ae051190934684b9a9c4a45f4dae27d13d3344
Angel Zhou <angel.zhou1016@gmail.com>
2022-11-23 13:31:26 -0500

    updates pet sitter index and show to work accordingly

--- src/components/pet_sitter/PetSitterShow.js
+++ src/components/pet_sitter/PetSitterShow.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { petSitterShow, petSitterDelete } from '../../api/petSitter'
 
+
 const PetSitterShow = ({ user, msgAlert }) => {
     const [petSitter, setPetSitter] = useState(null)
     const [deleted, setDeleted] = useState(false)
@@ -13,45 +14,46 @@ const PetSitterShow = ({ user, msgAlert }) => {
         window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
     }, []);
 
-    // useEffect(() => {
-    //     petSitterShow(user, id)
-    //         .then(res => {
-    //             setPetSitter(res.data.petSitter)
-    //         })
-    //         .catch((error) => {
-    //             msgAlert({
-    //                 heading: 'Failure',
-    //                 message: 'Show Pet Sitter Failed' + error,
-    //                 variant: 'danger'
-    //             })
-    //         })
-    // }, [updated])
+    useEffect(() => {
+        petSitterShow(user, id)
+            .then(res => {
+                console.log('Pet Show Page:', res.data)
+                setPetSitter(res.data.pet_sitter)
+            })
+            .catch((error) => {
+                msgAlert({
+                    heading: 'Failure',
+                    message: 'Show Pet Sitter Failed' + error,
+                    variant: 'danger'
+                })
+            })
+    }, [updated])
 
-    // const handleDeletePetSitter = () => {
-    //     petSitterDelete(user, id)
-    //         .then(() => {
-    //             setDeleted(true)
-    //             msgAlert({
-    //                 heading: 'Success',
-    //                 message: 'Deleting Pet Sitter Profile',
-    //                 variant: 'success'
-    //             })
-    //         })
-    //         .catch((error) => {
-    //             msgAlert({
-    //                 heading: 'Failure',
-    //                 message: 'Deleting Pet Sitter Profile Fail: ' + error,
-    //                 variant: 'danger'
-    //             })
-    //         })
-    // }
+    const handleDeletePetSitter = () => {
+        petSitterDelete(user, id)
+            .then(() => {
+                setDeleted(true)
+                msgAlert({
+                    heading: 'Success',
+                    message: 'Deleting Pet Sitter Profile',
+                    variant: 'success'
+                })
+            })
+            .catch((error) => {
+                msgAlert({
+                    heading: 'Failure',
+                    message: 'Deleting Pet Sitter Profile Fail: ' + error,
+                    variant: 'danger'
+                })
+            })
+    }
 
     // let reviewCards
     // if (petSitter) {
     //     if (petSitter.reviews.length > 0) {
     //         reviewCards = petSitter.reviews.map(review => (
-    //             <Container>
-    //                 <ShowReview
+    //             <div>
+    //                 <ReviewShow
     //                     key={review._id}
     //                     review={review}
     //                     petSitter={petSitter}
@@ -59,21 +61,25 @@ const PetSitterShow = ({ user, msgAlert }) => {
     //                     msgAlert={msgAlert}
     //                     triggerRefresh={() => setUpdated(prev => !prev)}
     //                 />
-    //             </Container>
+    //             </div>
     //         ))
     //     }
     // }
 
-    // if (deleted) navigate('/petsitters')
+    if (deleted) navigate('/petsitters')
 
-    // if (!petSitter) {
-    //     return <LoadingScreen />
-    // }
+    if (!petSitter) {
+        return (
+            <>
+                Loading
+            </>
+        )
+    }
 
 
     return (
         <>
-            Hello PetSitterShow
+            {petSitter.first_name} {petSitter.last_name}
         </>
     )
 }
