da973ef1db614dbaab229bdb17a5e0ffeef6c33d
Angel Zhou <angel.zhou1016@gmail.com>
2022-11-23 15:41:30 -0500

    updates data name to satisfy axios call to django

--- src/components/pet_sitter/PetSitterCreate.js
+++ src/components/pet_sitter/PetSitterCreate.js
@@ -46,7 +46,8 @@ const PetSitterCreate = ({ user, msgAlert }) => {
     const handleCreatePetSitter = event => {
         event.preventDefault()
         petSitterCreate(petSitter, user)
-            .then(res => { navigate(`/petsitters/${res.data.petSitter._id}`) })
+            .then(res => console.log('Create Pet Sitter:', res.data))
+            .then(res => { navigate(`/petsitters/${res.data.petSitter.id}`) })
             .then(() => {
                 msgAlert({
                     heading: 'Success',
