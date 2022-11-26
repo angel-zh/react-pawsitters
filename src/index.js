56520e89b0bf547d21b0282521fa932db7aad1aa
Angel Zhou <angel.zhou1016@gmail.com>
2022-11-26 11:31:05 -0500

    adds display flex

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { ProSidebarProvider } from 'react-pro-sidebar';
+import './index.css';
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ProSidebarProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    </ProSidebarProvider>,
   document.getElementById('root')
+  
 );
