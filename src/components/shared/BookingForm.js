5c0e13acd673c5a57b37ae7d8ca08175a71dc4a3
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-11-26 13:23:13 -0500

    can enter values into booking form

--- src/components/shared/BookingForm.js
+++ src/components/shared/BookingForm.js
@@ -6,26 +6,29 @@ const BookingForm = (props) => {
 
     return (
 
-        <Container className="justify-content-center rest-form" style={{ width: '48em', padding: '2rem' }} >
+        <Container className='justify-content-center' style={{ width: '48em', padding: '2rem' }} >
             <h3 className='mt-3'>{heading}</h3> 
             <Form onSubmit={handleSubmit}>
                 <Form.Label>Request Note:</Form.Label>
                 <Form.Control
-                    placeholder="A sentence or two about why you need a petsitter."
+                    placeholder='Please tell us the reason for your request.'
                     type='textbox'
-                    id="note"
+                    name='note'
+                    id='note'
                     value={booking.note}
                     onChange={handleChange}
                 />
 
-                <hr class="bg-success border-2 border-top border-success" />
+                {/* Might use this later, but not right now. */}
+                {/* <hr class="bg-success border-2 border-top border-success" /> */}
 
                 <Row>
                     <Form.Group as={Col}>
                         <Form.Label>Start Date:</Form.Label>
                         <Form.Control
                             type='date'
-                            id="start_day"
+                            name='start_day'
+                            id='start_day'
                             value={booking.start_day}
                             onChange={handleChange}
                         />
@@ -34,7 +37,8 @@ const BookingForm = (props) => {
                         <Form.Label>End Date:</Form.Label>
                         <Form.Control
                             type='date'
-                            id="end_day"
+                            name='end_day'
+                            id='end_day'
                             value={booking.end_day}
                             onChange={handleChange}
                         />
@@ -46,7 +50,8 @@ const BookingForm = (props) => {
                         <Form.Label>Start Time:</Form.Label>
                         <Form.Control
                             type='time'
-                            id="start_time"
+                            name='start_time'
+                            id='start_time'
                             value={booking.start_time}
                             onChange={handleChange}
                         />
@@ -56,7 +61,8 @@ const BookingForm = (props) => {
                         <Form.Label>End Time:</Form.Label>
                         <Form.Control
                             type='time'
-                            id="end_time"
+                            name='end_time'
+                            id='end_time'
                             value={booking.end_time}
                             onChange={handleChange}
                         />
