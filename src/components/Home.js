1e65a6594ccd1e9b2cb8a34bc7fe9bea4f441384
Angel Zhou <angel.zhou1016@gmail.com>
2022-11-29 13:51:04 -0500

    adds more info

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,14 @@ const Home = ({ user }) => {
 			<div className="homepage">
 				<h1 className="page-heading">Welcome to the PawSitters!</h1>
 				<h3>We are dedicated to making sure pawrents can easily and conveniently connect with our pawsitters to find care for their beloved pets of all shapes and sizes.</h3>
+				<h4 className='mt-3'>Our PawSitters provide a range of services:</h4>
+				<ul>
+					<li>Dog walking</li>
+					<li>Pet sitting (from the comfort of your home or at the awsitter's home)</li>
+					<li>Drop-in visits</li>
+					<li>Administration of pet medication</li>
+				</ul>
+				<p>Be sure to let our PawSitters know all the details when you fill out a booking request!</p>
 				<img src="https://i.imgur.com/NPgfqZb.jpg" alt="dog"></img>
 				<img src="https://i.imgur.com/vF1rrlJ.jpg" alt="cat"></img>
 				<img src="https://i.imgur.com/7ofQ8oe.jpg?2" alt="bird"></img>
@@ -24,7 +32,7 @@ const Home = ({ user }) => {
 						<div className='mx-4'>
 							<Link to='/sign-up' className='btn btn-outline-info mx-1'>Register a PawSitters Account</Link>
 							<Link to='/sign-in' className='btn btn-outline-info mx-1'>Sign Into Your PawSitters Account</Link>
-							<Link to='/petsitters' className='btn btn-outline-info mx-1'>Find Pet Sitters</Link>
+							<Link to='/petsitters' className='btn btn-outline-info mx-1'>Find PawSitters</Link>
 						</div>
 				}
 			</div>
