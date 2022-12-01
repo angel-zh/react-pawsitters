bf5a39e4b9f07f14f1af219958362f7c0a509903
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-12-01 13:14:48 -0500

    booking edit

--- src/components/booking/BookingUpdate.js
+++ src/components/booking/BookingUpdate.js
@@ -37,7 +37,7 @@ const BookingUpdate = (props) => {
             .then(() => {
                 setUpdated(true)
             })
-            .then(() => navigate('/bookings'))
+            .then(() => navigate('/dashboard'))
             .catch(() => {
                 navigate('/error')
             })
